0e9f6b90a45aaac7c04bd2e10e869f92d0a06dba
cdb339 <cdb339@drexel.edu>
2025-04-21 20:41:30 -0400

    add test file

---
+++ serial.py
@@ -0,0 +1,11 @@
+import serial
+while True:
+    try:
+        ser = serial.Serial('/dev/ttyS0', 115200, 1) 
+        # Ensure the port is correct and not already in use
+        print(f"Connected to: {ser.portstr}")
+        line = ser.readline().decode('utf-8').rstrip()
+        print(line)
+    except serial.SerialException as e:
+        print(f"Error opening serial port: {e}")
+        exit()
