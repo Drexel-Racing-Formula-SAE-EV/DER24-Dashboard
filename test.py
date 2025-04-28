95dd75c15ffc03d8b7b9ad27ade3406225640389
cdb339 <cdb339@drexel.edu>
2025-04-28 18:43:09 -0400

    test if serial port can send data

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
 import serial 
+
+port = serial.Serial("/dev/ttyAMA0", baudrate=115200, timeout=3.0)
 while True:
     try:
-        ser = serial.Serial('/dev/ttyS0', 115200, timeout=1) 
-        # Ensure the port is correct and not already in use
-        print(f"Connected to: {ser.portstr}")
-        line = ser.readline().decode('utf-8').rstrip()
-        print(line)
+        # ser = serial.Serial('/dev/ttyS0', 115200, timeout=1) 
+        # # Ensure the port is correct and not already in use
+        # print(f"Connected to: {ser.portstr}")
+        # line = ser.readline().decode('utf-8').rstrip()
+        # print(line)
+        port.write("\r\nSay something:")
+        rcv = port.read(10)
+        port.write("\r\nYou sent:" + repr(rcv))
+
     except serial.SerialException as e:
         print(f"Error opening serial port: {e}")
         exit()
